226d5a546eb8532800d088b9399096bf5959ca4d
Franime <franciscoimeroni@mail.com>
2024-04-02 11:07:49 -0300

    M2

--- back/src/config/dbCon.js
+++ back/src/config/dbCon.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbCon = async () => {
     //conexion con la base dedatos
-    await mongoose.connect("mongodb+srv://franciscoimeroni:morena007@proyectoprueba.sp2lfgq.mongodb.net/movies");
+    await mongoose.connect(URI);
 };
 
 module.exports = dbCon;
